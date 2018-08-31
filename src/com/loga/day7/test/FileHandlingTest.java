a760fd13b5db3ddd50b6c1e3b461b4bce86f948c
lsakthiv <lsakthiv@IN-MUM-LDVM026.corp.capgemini.com>
2018-08-31 05:33:55 -0400

    commit

---
+++ src/com/loga/day7/test/FileHandlingTest.java
@@ -0,0 +1,17 @@
+package com.loga.day7.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+
+import org.junit.jupiter.api.Test;
+
+class FileHandlingTest {
+
+	@Test
+	void testExistenceOfFile() {
+		File file = new File("C:\\Users\\lsakthiv\\Documents\\workspace-sts-3.9.5.RELEASE\\day2\\src\\com\\loga\\day2");
+		assertEquals(true, file.exists());
+		assertEquals(true, file.isFile());
+	}
+}
